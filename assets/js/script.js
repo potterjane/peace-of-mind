2c40a0ba0173c4e9a6c6830c951cfb414960580a
Jane Potter <tamblynstewart@gmail.com>
2021-05-14 13:12:35 +0000

    Added function names and comments in the script.js. Moved the HTML code for the welcome section from index.html to script.js.

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+function welcome() {
+    /*--When user enters page, Welcome function should appear for 5 seconds, with “How are you today?” typed out one letter at a time--*/
+
+    /*--This welcome section is only visible for 5 seconds when user enters the website, then is removed with a slide transition
+    <section id="welcome">
+    <div class="feeling">
+        <p>
+        How are you feeling today?  
+        </p>
+    </div>
+    </section>
+    /This welcome section is only visible for 5 seconds when user enters the website, then is removed with a slide transition--*/
+}
+
+function sounds() {
+    /*--At Home page, when user clicks on an icon, a random sound effect should be played until they click either “Reset all” or the icon again for pause. 
+    User should also be able to control the volume and change sound effect any time. Sound effect should be played in a loop in infinity until window is closed, 
+    user clicks “Reset all” or when user clicks pause.--*/
+}
+
+function resetAll() {
+    /*--When user clicks “Reset all”, all the sound effects and background are reset/turn off/cancelled--*/
+}
+
+function changeBackground() {
+    /*--When user clicks “Change background”, background changes--*/
+}
+
+function feedbackForm() {
+    /*--User can send in their questions or feedback via “Contact” page, all fields are required before they can submit the form--*/
+}
