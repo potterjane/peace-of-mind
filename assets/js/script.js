36649505117bfcb4b3d9fc8d1c4dd36bb04c9072
Jane Potter <tamblynstewart@gmail.com>
2021-05-23 23:01:25 +0000

    Added JavaScript functions and CSS stylings to the Welcome section, which is the splash/intro page that appears for 5 seconds when user enters the website.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,34 @@
-function welcome() {
-    /*--When user enters page, Welcome function should appear for 5 seconds, with “How are you today?” typed out one letter at a time--*/
+/*--When user enters page, Welcome function should appear for 5 seconds, with “How are you today?” typed out one letter at a time--*/
 
-    /*--This welcome section is only visible for 5 seconds when user enters the website, then is removed with a slide transition
-    <section id="welcome">
-    <div class="feeling">
-        <p>
-        How are you feeling today?  
-        </p>
-    </div>
-    </section>
-    /This welcome section is only visible for 5 seconds when user enters the website, then is removed with a slide transition--*/
+/*--Code taken from Conor Bailey's instructional Youtube video on how to place welcome text in front of the website for 5 seconds--*/
+const welcome = document.querySelector('.welcome');
+
+document.addEventListener('DOMContentLoaded', (e)=> {
+    setTimeout(()=>{
+        welcome.classList.add('displayNone');
+    }, 5000);
+})
+
+/*--/Code taken from Conor Bailey's instructional Youtube video on how to place welcome text in front of the website for 5 seconds--*/
+
+/*--Code taken from Maximus McCullough's Youtube instructional video on how to use JS to type in one letter at a time--*/
+let feelingText = "How are you feeling today?";
+let feelingArray = feelingText.split("");
+let feelingTimer;
+
+function feeling() {
+    if (feelingArray.length > 0) {
+        document.getElementById('feeling').innerHTML += feelingArray.shift();
+    } else {
+        clearTimeout(feelingTimer);
+        return false;
+    }
+    feelingTimer = setTimeout('feeling()', 110);
 }
+
+feeling();
+/*--/Code taken from Maximus McCullough's Youtube instructional video on how to use JS to type in one letter at a time--*/
+
 
 function sounds() {
     /*--At Home page, when user clicks on an icon, a random sound effect should be played until they click either “Reset all” or the icon again for pause. 
@@ -29,10 +47,8 @@ function changeBackground() {
 /* --------------------------------------------------------------------------------------------------------------------------------------------Contact page */
 
 /*--User can send in their questions or feedback via “Contact” page, all fields are required before they can submit the form--*/
-function feedbackForm(contactForm) { 
-
 /*--All the code contained in this function was taken from Code Institute's Sending Emails Using EmailJS module--*/
-
+function feedbackForm(contactForm) { 
     emailjs.send('Gmail', 'pom', {
         'from_name': contactForm.name.value,
         'from_email': contactForm.email.value,
@@ -47,5 +63,5 @@ function feedbackForm(contactForm) {
         });
     return false;
 
-/*--/All the code contained in this function was taken from Code Institute's Sending Emails Using EmailJS module--*/
 }
+/*--/All the code contained in this function was taken from Code Institute's Sending Emails Using EmailJS module--*/
