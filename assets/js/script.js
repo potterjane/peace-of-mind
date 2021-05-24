21b409661bfba0dbf00117d2d5e5f0ac97314368
Jane Potter <tamblynstewart@gmail.com>
2021-05-24 18:00:57 +0000

    Removed the Welcome section in HTML, CSS and JavaScript page respectively to see if it fixes the bug of the index.html page not working, navbar, icons and button are unclickable.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,5 @@
 /* --------------------------------------------------------------------------------------------------------------------------------------------Welcome section */
-/*--When user enters page, Welcome function should appear for 5 seconds, with “How are you today?” typed out one letter at a time--*/
 
-/*--Code taken from Conor Bailey's instructional Youtube video on how to place welcome text in front of the website for 5 seconds--*/
-const welcome = document.querySelector('.welcome');
-
-document.addEventListener('DOMContentLoaded', (e)=> {
-    setTimeout(()=>{
-        welcome.classList.add('displayNone');
-    }, 5000);
-})
-
-/*--/Code taken from Conor Bailey's instructional Youtube video on how to place welcome text in front of the website for 5 seconds--*/
-
-/*--Code taken from Maximus McCullough's Youtube instructional video on how to use JS to type in one letter at a time--*/
-let feelingText = "How are you feeling today?";
-
-let feelingArray = feelingText.split("");
-
-let feelingTimer;
-
-function feeling() {
-    if (feelingArray.length > 0) {
-        document.getElementById('feeling').innerHTML += feelingArray.shift();
-    } else {
-        clearTimeout(feelingTimer);
-        return false;
-    }
-    feelingTimer = setTimeout('feeling()', 110);
-}
-
-feeling();
-/*--/Code taken from Maximus McCullough's Youtube instructional video on how to use JS to type in one letter at a time--*/
 
 /* --------------------------------------------------------------------------------------------------------------------------------------------Audio section */
 function sounds() {
