6ed689c62fb9b34cc5b5c286bff0c391e90857a7
Jane Potter <tamblynstewart@gmail.com>
2021-05-24 18:59:44 +0000

    When the index.html bug was fixed, the HTML, CSS and Javascript code for Welcome section was added back, adjusted the div for background videos to remove the bug, added if/else statement to make the Welcome section only appear for user once per visit.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,38 @@
 /* --------------------------------------------------------------------------------------------------------------------------------------------Welcome section */
+//When user enters page, Welcome function should appear for 5 seconds, with “How are you today?” typed out one letter at a time
 
+//Code taken from Conor Bailey's instructional Youtube video on how to place welcome text in front of the website for 5 seconds
+const welcome = document.querySelector('.welcome');
+
+if (!sessionStorage.isVisited) { //Got this if/else and sessionStorage code from SitePoint, this helps only show the preloader once per visit
+    sessionStorage.isVisited = 'true';
+    document.addEventListener('DOMContentLoaded', (e)=> {
+    setTimeout(()=>{
+        welcome.classList.add('displayNone');
+    }, 5000);
+    })
+} else {
+    $('.welcome').hide();
+}
+
+//Code taken from Maximus McCullough's Youtube instructional video on how to use JS to type in one letter at a time
+let feelingText = "How are you feeling today?";
+
+let feelingArray = feelingText.split("");
+
+let feelingTimer;
+
+function feeling() {
+    if (feelingArray.length > 0) {
+        document.getElementById('feeling').innerHTML += feelingArray.shift();
+    } else {
+        clearTimeout(feelingTimer);
+        return false;
+    }
+    feelingTimer = setTimeout('feeling()', 110);
+}
+
+feeling();
 
 /* --------------------------------------------------------------------------------------------------------------------------------------------Audio section */
 function sounds() {
