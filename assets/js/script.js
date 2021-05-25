3ca17988ec1e81e46a909ca00c118c24bc29627b
Jane Potter <tamblynstewart@gmail.com>
2021-05-25 13:59:30 +0000

    Added mp4 and webm video sources in an array list in Javascript page. Worked on the onclick function for these videos to use as background choices.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-/* --------------------------------------------------------------------------------------------------------------------------------------------Welcome section */
+//--------------------------------------------------------------------------------------------------------------------------------------------------Welcome section
 //When user enters page, Welcome function should appear for 5 seconds, with “How are you today?” typed out one letter at a time
 
 //Code taken from Conor Bailey's instructional Youtube video on how to place welcome text in front of the website for 5 seconds
@@ -34,39 +34,89 @@ function feeling() {
 
 feeling();
 
-/* --------------------------------------------------------------------------------------------------------------------------------------------Audio section */
+//----------------------------------------------------------------------------------------------------------------------------------------------------Audio section
+//At Home page, when user clicks on an icon, a random sound effect should be played until they click either “Reset all” or the icon again for pause. 
+//User should also be able to control the volume and change sound effect any time. 
+//Sound effect should be played in a loop in infinity until window is closed, user clicks “Reset all” or when user clicks pause.
+
 function sounds() {
-    /*--At Home page, when user clicks on an icon, a random sound effect should be played until 
-    they click either “Reset all” or the icon again for pause. 
-    User should also be able to control the volume and change sound effect any time. Sound effect 
-    should be played in a loop in infinity until window is closed, 
-    user clicks “Reset all” or when user clicks pause.--*/
+    
 }
 
-/* --------------------------------------------------------------------------------------------------------------------------------------------Reset section */
+//----------------------------------------------------------------------------------------------------------------------------------------------------Reset section
+//When user clicks “Reset all”, all the sound effects and background are reset/turn off/cancelled
+
 function resetAll() {
-    /*--When user clicks “Reset all”, all the sound effects and background are reset/turn off/cancelled--*/
+
 }
 
-/* --------------------------------------------------------------------------------------------------------------------------------------------Video section */
-/*--When user clicks “Change background” button, background changes--*/
+//----------------------------------------------------------------------------------------------------------------------------------------------------Video section
+//When user clicks “Change background” button, background changes
 
-let changeBtn = document.getElementsByClassName('change');
-let sources = document.getElementsByTagName('source');
+let videos = [
+    {
+        sources: "assets/videos/mp4/beach-waves-move.mp4",
+        types: "video/mp4"
+    },
+    {
+        sources: "assets/videos/mp4/fireplace.mp4",
+        types: "video/mp4"
+    },
+    {
+        sources: "assets/videos/mp4/forest.mp4",
+        types: "video/mp4"
+    },
+    {
+        sources: "assets/videos/mp4/office.mp4",
+        types: "video/mp4"
+    },
+    {
+        sources: "assets/videos/mp4/raindrops.mp4",
+        types: "video/mp4"
+    },
+    {
+        sources: "assets/videos/mp4/reading.mp4",
+        types: "video/mp4"
+    },
+    {
+        sources: "assets/videos/webm/beach-waves-move.webm",
+        types: "video/webm"
+    },
+    {
+        sources: "assets/videos/webm/fireplace.webm",
+        types: "video/webm"
+    },
+    {
+        sources: "assets/videos/webm/forest.webm",
+        types: "video/webm"
+    },
+    {
+        sources: "assets/videos/webm/office.webm",
+        types: "video/webm"
+    },
+    {
+        sources: "assets/videos/webm/raindrops.webm",
+        types: "video/webm"
+    },
+    {
+        sources: "assets/videos/webm/reading.webm",
+        types: "video/webm"
+    },
+];
 
-changeBtn.addEventListener('click', changeBackground);
+let sources = document.getElementsByTagName('source').getAttribute("src"); 
+let types = document.getElementsByTagName('source').getAttribute("type");
 
-function changeBackground(event) {
-    for (i = 0; i <= sources.length; i++) {
-        sources[i];
+function changeBackground() {
+    for (let i = 0; i <= videos.length; i++) {
+        videos[i];
     };
 }
-changeBackground();
 
-/* --------------------------------------------------------------------------------------------------------------------------------------------Contact section */
+//--------------------------------------------------------------------------------------------------------------------------------------------------Contact section
+//User can send in their questions or feedback via “Contact” page, all fields are required before they can submit the form
 
-/*--User can send in their questions or feedback via “Contact” page, all fields are required before they can submit the form--*/
-/*--All the code contained in this function was taken from Code Institute's Sending Emails Using EmailJS module--*/
+//All the code contained in this function was taken from Code Institute's Sending Emails Using EmailJS module
 function feedbackForm(contactForm) { 
     emailjs.send('Gmail', 'pom', {
         'from_name': contactForm.name.value,
@@ -81,6 +131,4 @@ function feedbackForm(contactForm) {
             console.log('FAILED', error);
         });
     return false;
-
 }
-/*--/All the code contained in this function was taken from Code Institute's Sending Emails Using EmailJS module--*/
