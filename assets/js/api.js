bb330e468e99922a03c3682804e8cfd43ca0f948
Jane Potter <tamblynstewart@gmail.com>
2021-05-31 21:50:18 +0000

    Created favicon for the website, added links to the favicon in every head section in the HTML pages. Added Spotify API client ID and client secret in the api.js page. Reduced the font size of the Welcome header for mobile screen sizes.

---
+++ assets/js/api.js
@@ -0,0 +1,4 @@
+//Code on how to use Spotify API was learned from Avery Wicks' instructional Youtube video
+const clientID = "05f1a869d9c24b8dbce0d0c444ca7e84";
+const clientSecret = "132cb8a24c914ee28178553dd96d326f";
+
