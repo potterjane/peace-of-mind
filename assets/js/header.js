68cfa65e8a0aaeb97b3769f3133426c3f2cd00de
Jane Potter <tamblynstewart@gmail.com>
2021-06-02 21:44:31 +0000

    Removed the unused style codes in the header.js page.

--- assets/js/header.js
+++ assets/js/header.js
@@ -3,15 +3,9 @@
 function openNav() {
     document.getElementById("mobSidebar").style.width = "250px";
     document.getElementById("header").style.marginLeft = "250px";
-    document.getElementById("small-title").style.marginLeft = "250px";
-    document.getElementById("main-home").style.marginLeft = "250px";
-    document.getElementById("footer-home").style.marginLeft = "250px";
 }
   
 function closeNav() {
     document.getElementById("mobSidebar").style.width = "0";
     document.getElementById("header").style.marginLeft= "0";
-    document.getElementById("small-title").style.marginLeft = "0";
-    document.getElementById("main-home").style.marginLeft = "0";
-    document.getElementById("footer-home").style.marginLeft = "0";
 }
