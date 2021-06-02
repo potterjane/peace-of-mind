7e28311f47acd8c8369d8bc43bd968ae5fb88279
Jane Potter <tamblynstewart@gmail.com>
2021-06-02 21:47:18 +0000

    Removed the jokes API page and replaced it with quotes API page. Added function and styling to the quote section which is located in the index.html. Also changed the main background from turquoise color to an image background.

---
+++ assets/js/quotes_api.js
@@ -0,0 +1,18 @@
+/*
+* User clicks the 'Want inspiration?' button to get a random inspirational quotes
+* Got help from SoloLearn on how to get data from API
+* Quotes sourced from 'Quotes Free API'
+*/
+
+document.addEventListener("click", function (event) {
+    if (!event.target.matches("#quotesBtn")) return; //Checking if the button was clicked
+    
+    fetch("https://type.fit/api/quotes") //Using fetch API to request the endpoint for random quotes
+        .then(response => response.json()) //fetch returns a response object which we pass to a JavaScript object
+        .then(data=> { //Result of that data gets passed to the innerHTML below
+            var randomIndex = Math.floor(Math.random()*data.length);
+            document.getElementById("text").innerHTML = '<i class="fas fa-quote-left"></i> <br>'+data[randomIndex].text;
+            document.getElementById("author").innerHTML = '-' + data[randomIndex].author;
+    });
+    
+});
