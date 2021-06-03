a7fa8ba4f6a19876d0bd86431e563e990dea8e70
Jane Potter <tamblynstewart@gmail.com>
2021-06-03 19:45:08 +0000

    Ran the weather.js page on JSHint, adjusted the JS code to remove the warnings and added more structured comments.

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,21 +1,23 @@
-//At Home page, when user clicks on an icon, a random sound effect should be played until they click either “Reset all” or the icon again for pause. 
-//User should also be able to control the volume and change sound effect any time. 
-//Sound effect should be played in a loop in infinity until window is closed, or user clicks “Reset all”, or when user clicks pause.
+/*
+* At Home page, when user clicks on an icon, a random sound effect should be played until they click either “Reset all” or the icon again for pause. 
+* User should also be able to control the volume and change sound effect any time. 
+* Sound effect should be played in a loop in infinity until window is closed, or user clicks “Reset all”, or when user clicks pause.
+*/
 
 //Weather sounds
-let weathers = [
+var weathers = [
     "assets/audio/weather/rain.mp3",
     "assets/audio/weather/thunder.mp3"
 ];
 
-let sourcesWeather = document.getElementById('sourceWeather');
-let weatherSound = document.getElementById('weather-sound');
-let weatherIcon = document.getElementById('weather-icon');
+var sourcesWeather = document.getElementById('sourceWeather');
+var weatherSound = document.getElementById('weather-sound');
+var weatherIcon = document.getElementById('weather-icon');
 
 weatherIcon.addEventListener('click', toggleWeather); 
 
 function toggleWeather() {
-    let weatherAudio = weathers[Math.floor(Math.random() * weathers.length)]; //Got a random audio file from weathers-array
+    var weatherAudio = weathers[Math.floor(Math.random() * weathers.length)]; //Got a random audio file from weathers-array
     sourcesWeather.setAttribute("src", weatherAudio);
     
     if (weatherSound.paused) {
@@ -31,7 +33,7 @@ function toggleWeather() {
 }
 
 function randomWeather() {
-    let weatherAudio = weathers[Math.floor(Math.random() * weathers.length)]; 
+    var weatherAudio = weathers[Math.floor(Math.random() * weathers.length)]; 
     sourcesWeather.setAttribute("src", weatherAudio);
 
     for (var i = 0; i < weathers.length; i++) {
@@ -42,10 +44,13 @@ function randomWeather() {
     }
 }
 
-//Weather volume control
-//Code taken from StackOverflow on how to add volume control function in JavaScript
-let volumeWeather = document.querySelector("#volume-weather");
+/* 
+* Weather volume control
+* Code taken from StackOverflow on how to add volume control function in JavaScript
+*/
+
+var volumeWeather = document.querySelector("#volume-weather");
 
 volumeWeather.addEventListener("change", function(e) {
 weatherSound.volume = e.currentTarget.value / 100;
-})
+});
