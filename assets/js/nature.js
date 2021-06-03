2f9f04b9dedafca85e441f8f4ae6d1c2efff466f
Jane Potter <tamblynstewart@gmail.com>
2021-06-03 19:10:24 +0000

    Ran the nature.js page on JSHint, adjusted the JS code to remove the warnings and added more structured comments.

--- assets/js/nature.js
+++ assets/js/nature.js
@@ -1,22 +1,24 @@
-//At Home page, when user clicks on an icon, a random sound effect should be played until they click either “Reset all” or the icon again for pause. 
-//User should also be able to control the volume and change sound effect any time. 
-//Sound effect should be played in a loop in infinity until window is closed, or user clicks “Reset all”, or when user clicks pause.
+/*
+* At Home page, when user clicks on an icon, a random sound effect should be played until they click either “Reset all” or the icon again for pause. 
+* User should also be able to control the volume and change sound effect any time. 
+* Sound effect should be played in a loop in infinity until window is closed, or user clicks “Reset all”, or when user clicks pause.
+*/
 
 //Nature sounds
-let natures = [
+var natures = [
     "assets/audio/nature/birds.mp3",
     "assets/audio/nature/leaves.mp3",
     "assets/audio/nature/waves.mp3"
 ];
 
-let sourcesNature = document.getElementById('sourceNature');
-let natureSound = document.getElementById('nature-sound');
-let natureIcon = document.getElementById('nature-icon');
+var sourcesNature = document.getElementById('sourceNature');
+var natureSound = document.getElementById('nature-sound');
+var natureIcon = document.getElementById('nature-icon');
 
 natureIcon.addEventListener('click', toggleNature); 
 
 function toggleNature() {
-    let natureAudio = natures[Math.floor(Math.random() * natures.length)]; //Got a random audio file from natures-array
+    var natureAudio = natures[Math.floor(Math.random() * natures.length)]; //Got a random audio file from natures-array
     sourcesNature.setAttribute("src", natureAudio);
     
     if (natureSound.paused) {
@@ -32,7 +34,7 @@ function toggleNature() {
 }
 
 function randomNature() {
-    let natureAudio = natures[Math.floor(Math.random() * natures.length)];
+    var natureAudio = natures[Math.floor(Math.random() * natures.length)];
     sourcesNature.setAttribute("src", natureAudio);
 
     for (var i = 0; i < natures.length; i++) {
@@ -43,10 +45,13 @@ function randomNature() {
     }
 }
 
-//Nature volume control
-//Code taken from StackOverflow on how to add volume control function in JavaScript
-let volumeNature = document.querySelector("#volume-nature");
+/*
+* Nature volume control
+* Code taken from StackOverflow on how to add volume control function in JavaScript
+*/
+
+var volumeNature = document.querySelector("#volume-nature");
 
 volumeNature.addEventListener("change", function(e) {
 natureSound.volume = e.currentTarget.value / 100;
-})
+});
