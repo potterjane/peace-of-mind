cbf526ad9e15028029078176340c3845a21ace2a
Jane Potter <tamblynstewart@gmail.com>
2021-06-03 19:22:09 +0000

    Ran the quotes_api.js page on JSHint, adjusted the JS code to remove the warnings and added more structured comments.

--- assets/js/quotes_api.js
+++ assets/js/quotes_api.js
@@ -8,11 +8,13 @@ document.addEventListener("click", function (event) {
     if (!event.target.matches("#quotesBtn")) return; //Checking if the button was clicked
     
     fetch("https://type.fit/api/quotes") //Using fetch API to request the endpoint for random quotes
-        .then(response => response.json()) //fetch returns a response object which we pass to a JavaScript object
-        .then(data=> { //Result of that data gets passed to the innerHTML below
-            var randomIndex = Math.floor(Math.random()*data.length);
-            document.getElementById("text").innerHTML = '<i class="fas fa-quote-left"></i> <br>'+data[randomIndex].text;
-            document.getElementById("author").innerHTML = '-' + data[randomIndex].author;
+        .then(function(response) {
+            return response.json(); //fetch returns a response object which we pass to a JavaScript object
+        })
+        .then(function(data) { //Result of that data gets passed to the innerHTML below
+        var randomIndex = Math.floor(Math.random()*data.length);
+        document.getElementById("text").innerHTML = '<i class="fas fa-quote-left"></i> <br>'+data[randomIndex].text;
+        document.getElementById("author").innerHTML = '-' + data[randomIndex].author;
     });
     
 });
