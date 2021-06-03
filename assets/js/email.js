b6b403b8040d8888112450fd26301fca78e30a69
Jane Potter <tamblynstewart@gmail.com>
2021-06-03 18:32:27 +0000

    Ran the email.js page on JSHint, adjusted the JS code to remove the warnings and added more structured comments. Also added alerts to the functions to inform user when the message has successfully been sent or vice versa.

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,4 +1,7 @@
-//User can send in their questions or feedback via “Contact” page, all fields except upload field are required before they can submit the form
+/*
+*User can send in their questions or feedback via “Contact” page, all fields 
+except upload field are required before they can submit the form
+*/
 
 //All the code contained in this function was taken from Code Institute's Sending Emails Using EmailJS module
 function feedbackForm(contactForm) { 
@@ -9,12 +12,14 @@ function feedbackForm(contactForm) {
     })
     .then(
         function(response) {
+            alert("Message successfully sent!");
             console.log('SUCCESS', response);
         },
         function(error) {
+            alert("Message failed to send.");
             console.log('FAILED', error);
         });
 
-    contactForm.reset() //Code taken from W3Schools to clear form after every submit
+    contactForm.reset(); //Code taken from W3Schools to clear form after every submit
     return false;
 }
