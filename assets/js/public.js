96da5d18ddf7e8f9a40fbf8fab12a4cdb7e100b0
Jane Potter <tamblynstewart@gmail.com>
2021-06-03 19:13:07 +0000

    Ran the public.js page on JSHint, adjusted the JS code to remove the warnings and added more structured comments.

--- assets/js/public.js
+++ assets/js/public.js
@@ -1,22 +1,24 @@
-//At Home page, when user clicks on an icon, a random sound effect should be played until they click either “Reset all” or the icon again for pause. 
-//User should also be able to control the volume and change sound effect any time. 
-//Sound effect should be played in a loop in infinity until window is closed, or user clicks “Reset all”, or when user clicks pause.
+/*
+* At Home page, when user clicks on an icon, a random sound effect should be played until they click either “Reset all” or the icon again for pause. 
+* User should also be able to control the volume and change sound effect any time. 
+* Sound effect should be played in a loop in infinity until window is closed, or user clicks “Reset all”, or when user clicks pause.
+*/
 
 //Public sounds
-let publics = [
+var publics = [
     "assets/audio/public/office.mp3",
     "assets/audio/public/restaurant.mp3",
     "assets/audio/public/traffic.mp3"
 ];
 
-let sourcesPublic = document.getElementById('sourcePublic');
-let publicSound = document.getElementById('public-sound');
-let publicIcon = document.getElementById('public-icon');
+var sourcesPublic = document.getElementById('sourcePublic');
+var publicSound = document.getElementById('public-sound');
+var publicIcon = document.getElementById('public-icon');
 
 publicIcon.addEventListener('click', togglePublic); 
 
 function togglePublic() {
-    let publicAudio = publics[Math.floor(Math.random() * publics.length)]; //Got a random audio file from publics-array
+    var publicAudio = publics[Math.floor(Math.random() * publics.length)]; //Got a random audio file from publics-array
     sourcesPublic.setAttribute("src", publicAudio);
     
     if (publicSound.paused) {
@@ -32,7 +34,7 @@ function togglePublic() {
 }
 
 function randomPublic() {
-    let publicAudio = publics[Math.floor(Math.random() * publics.length)];
+    var publicAudio = publics[Math.floor(Math.random() * publics.length)];
     sourcesPublic.setAttribute("src", publicAudio);
 
     for (var i = 0; i < publics.length; i++) {
@@ -43,10 +45,13 @@ function randomPublic() {
     }
 }
 
-//Public volume control
-//Code taken from StackOverflow on how to add volume control function in JavaScript
-let volumePublic = document.querySelector("#volume-public");
+/*
+* Public volume control
+* Code taken from StackOverflow on how to add volume control function in JavaScript
+*/
+
+var volumePublic = document.querySelector("#volume-public");
 
 volumePublic.addEventListener("change", function(e) {
 publicSound.volume = e.currentTarget.value / 100;
-})
+});
