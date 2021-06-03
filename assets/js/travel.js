ce82897a37f4918480c2b449be3fbda5ca56548b
Jane Potter <tamblynstewart@gmail.com>
2021-06-03 19:26:48 +0000

    Ran the travel.js page on JSHint, adjusted the JS code to remove the warnings and added more structured comments.

--- assets/js/travel.js
+++ assets/js/travel.js
@@ -1,22 +1,24 @@
-//At Home page, when user clicks on an icon, a random sound effect should be played until they click either “Reset all” or the icon again for pause. 
-//User should also be able to control the volume and change sound effect any time. 
-//Sound effect should be played in a loop in infinity until window is closed, or user clicks “Reset all”, or when user clicks pause.
+/*
+* At Home page, when user clicks on an icon, a random sound effect should be played until they click either “Reset all” or the icon again for pause. 
+* User should also be able to control the volume and change sound effect any time. 
+* Sound effect should be played in a loop in infinity until window is closed, or user clicks “Reset all”, or when user clicks pause.
+*/
 
 //Travel sounds
-let travels = [
+var travels = [
     "assets/audio/travel/bicycle.mp3",
     "assets/audio/travel/car.mp3",
     "assets/audio/travel/train.mp3"
 ];
 
-let sourcesTravel = document.getElementById('sourceTravel');
-let travelSound = document.getElementById('travel-sound');
-let travelIcon = document.getElementById('travel-icon');
+var sourcesTravel = document.getElementById('sourceTravel');
+var travelSound = document.getElementById('travel-sound');
+var travelIcon = document.getElementById('travel-icon');
 
 travelIcon.addEventListener('click', toggleTravel); 
 
 function toggleTravel() {
-    let travelAudio = travels[Math.floor(Math.random() * travels.length)]; //Got a random audio file from travels-array
+    var travelAudio = travels[Math.floor(Math.random() * travels.length)]; //Got a random audio file from travels-array
     sourcesTravel.setAttribute("src", travelAudio);
     
     if (travelSound.paused) {
@@ -32,7 +34,7 @@ function toggleTravel() {
 }
 
 function randomTravel() {
-    let travelAudio = travels[Math.floor(Math.random() * travels.length)]; 
+    var travelAudio = travels[Math.floor(Math.random() * travels.length)]; 
     sourcesTravel.setAttribute("src", travelAudio);
 
     for (var i = 0; i < travels.length; i++) {
@@ -43,10 +45,13 @@ function randomTravel() {
     }
 }
 
-//Travel volume control
-//Code taken from StackOverflow on how to add volume control function in JavaScript
-let volumeTravel = document.querySelector("#volume-travel");
+/*
+* Travel volume control
+* Code taken from StackOverflow on how to add volume control function in JavaScript
+*/
+
+var volumeTravel = document.querySelector("#volume-travel");
 
 volumeTravel.addEventListener("change", function(e) {
     travelSound.volume = e.currentTarget.value / 100;
-})
+});
