28dc15d26a562c7182b74770e321772e84af4cb0
Jane Potter <tamblynstewart@gmail.com>
2021-06-06 07:44:54 +0000

    Added more comments to email.js

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,22 +1,26 @@
 /*
-*User can send in their questions or feedback via “Contact” page, all fields 
+User can send in their questions or feedback via “Contact” page, all fields 
 except upload field are required before they can submit the form
 */
 
-//All the code contained in this function was taken from Code Institute's Sending Emails Using EmailJS module
+/* 
+All the code contained in this function was taken from Code Institute's 
+Sending Emails Using EmailJS module
+*/
+
 function feedbackForm(contactForm) { 
-    emailjs.send('Gmail', 'pom', {
-        'from_name': contactForm.name.value,
-        'from_email': contactForm.email.value,
-        'message': contactForm.message.value
+    emailjs.send('Gmail', 'pom', { //Service ID(Gmail), Template ID(pom, i.e. for peace of mind)
+        'from_name': contactForm.name.value, //Get the value from the text input and add it to the email template
+        'from_email': contactForm.email.value, //Get the value from the email input and add it to the email template
+        'message': contactForm.message.value //Get the value from the message textarea and add it to the email template
     })
     .then(
         function(response) {
-            alert("Message successfully sent!");
+            alert("Message successfully sent!"); //Alert users with a pop-up message when message was sent sucessfully
             console.log('SUCCESS', response);
         },
         function(error) {
-            alert("Message failed to send.");
+            alert("Message failed to send."); //Alert users with a pop-up message when message failed to send
             console.log('FAILED', error);
         });
 
