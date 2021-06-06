d2979397e8930dbb80969fbe99b5805a8b84b39a
Jane Potter <tamblynstewart@gmail.com>
2021-06-06 07:55:17 +0000

    Added more comments to header.js

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,11 +1,11 @@
 //Header section for smaller screens, collapsed sidebar code taken from W3Schools
 
-function openNav() {
-    document.getElementById("mobSidebar").style.width = "250px";
-    document.getElementById("header").style.marginLeft = "250px";
+function openNav() { //Function that opens the sidebar when user clicks the '☰'
+    document.getElementById("mobSidebar").style.width = "250px"; //Moves the navigation links section to the right with 250px
+    document.getElementById("header").style.marginLeft = "250px"; //Moves the '☰' to the right with 250px
 }
   
-function closeNav() {
-    document.getElementById("mobSidebar").style.width = "0";
-    document.getElementById("header").style.marginLeft= "0";
+function closeNav() { //Function that closes the sidebar when user clicks the 'x'
+    document.getElementById("mobSidebar").style.width = "0"; //Moves the navigation links section back to the left from 250px to 0
+    document.getElementById("header").style.marginLeft= "0"; //Moves the '☰' back to the left from 250px to 0
 }
