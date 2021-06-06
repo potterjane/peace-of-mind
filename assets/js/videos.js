a5b0e22c7d9cccf5eae7e073f99e12ed33dc1033
Jane Potter <tamblynstewart@gmail.com>
2021-06-06 09:40:43 +0000

    Added more comments in videos.js

--- assets/js/videos.js
+++ assets/js/videos.js
@@ -1,6 +1,6 @@
 //When user clicks “Change background” button, background changes to a random video 
 
-//Arraylist of videos with src and type
+//Videos-array with src and type
 var videos = [
     {
         sources: "assets/videos/mp4/aurora-borealis.mp4",
@@ -50,68 +50,18 @@ var videos = [
         sources: "assets/videos/mp4/sunset-by-beach.mp4",
         types: "video/mp4"
     }
-    /*
-    {
-        sources: "assets/videos/webm/aurora-borealis.webm",
-        types: "video/webm"
-    },
-    {
-        sources: "assets/videos/webm/beach-waves-move.webm",
-        types: "video/webm"
-    },
-    {
-        sources: "assets/videos/webm/car-drive.webm",
-        types: "video/webm"
-    },
-    {
-        sources: "assets/videos/webm/fireplace.webm",
-        types: "video/webm"
-    },
-    {
-        sources: "assets/videos/webm/forest.webm",
-        types: "video/webm"
-    },
-    {
-        sources: "assets/videos/webm/night-sky.webm",
-        types: "video/webm"
-    },
-    {
-        sources: "assets/videos/webm/office.webm",
-        types: "video/webm"
-    },
-    {
-        sources: "assets/videos/webm/pink-sky-and-beach.webm",
-        types: "video/webm"
-    },
-    {
-        sources: "assets/videos/webm/rain-on-banana-leaves.webm",
-        types: "video/webm"
-    },
-    {
-        sources: "assets/videos/webm/raindrops.webm",
-        types: "video/webm"
-    },
-    {
-        sources: "assets/videos/webm/reading.webm",
-        types: "video/webm"
-    },
-    {
-        sources: "assets/videos/webm/sunset-by-beach.webm",
-        types: "video/webm"
-    }
-    */
 ];
 
-var sourcesVid = document.getElementById('sourceVideos');
-var bgVideo = document.getElementById('bg-video');
+var sourcesVid = document.getElementById('sourceVideos'); //Source id taken from index.html
+var bgVideo = document.getElementById('bg-video'); //Video id taken from index.html
 
-function changeBackground() {
-    var bgVideos = videos[Math.floor(Math.random() * videos.length)]; //Code got from Kirupa
-    var randomChoice = Object.values(bgVideos); //Got code from Javascript.info
-    sourcesVid.setAttribute("src", randomChoice[0]); //Got set attribute from W3Schools
-    sourcesVid.setAttribute("type", randomChoice[1]);
+function changeBackground() { //Background video function
+    var bgVideos = videos[Math.floor(Math.random() * videos.length)]; //Got a random video file from videos-array, code got from Kirupa
+    var randomChoice = Object.values(bgVideos); //Got this method to return the videos-array values from Javascript.info
+    sourcesVid.setAttribute("src", randomChoice[0]); //Adds random videos to the src attribute in index.html, got set attribute from W3Schools
+    sourcesVid.setAttribute("type", randomChoice[1]); //Adds random videos to the type attribute in index.html, got set attribute from W3Schools
 
-    $("div").removeClass("cover-image"); //Got removeClass code from W3Schools
+    $("div").removeClass("cover-image"); //When user clicks 'Change background' button, main background image is removed, got removeClass code from W3Schools
     bgVideo.load();
     bgVideo.play();
 }
